413587f5da203c469e0b3fef0d6290ea89980713
MR <alledaten@daum.net>
2024-05-24 21:17:57 +0900

    add Indef insane Check api

--- API/indefInsaneCheck.js
+++ API/indefInsaneCheck.js
@@ -1,43 +1,53 @@
 let cha_ids = [];
 
-on("ready", function () {
-    const cha_id = findObjs({type: "character"});
-    for (let i = 0; i < cha_id.length; i++) {
-        const ctrlby = cha_id[i].get('controlledby');
+on("ready", function() {
+    let cha_id = findObjs({type:"character"});
+    for (var i=0; i<cha_id.length; i++) {
+        let ctrlby = cha_id[i].get('controlledby');
         if (!playerIsGM(ctrlby) && ctrlby.length > 0) {
-            cha_ids.push(cha_id[i].get('_id'));
+            let id = cha_id[i].get('_id')
+            cha_ids.push(id);
         }
     }
 });
 
-on("chat:message", function (msg) {
-    if (msg.type === "api") {
-        if (msg.content === "!!daypassed") {
-            let check = false;
-            for (let i = 0; i < cha_ids.length; i++) {
-                const cha_san = findObjs({type: "attribute", name: "san", characterid: cha_ids[i]});
-                const cha_sanStart = findObjs({type: "attribute", name: "san_start", characterid: cha_ids[i]});
-                const cha_sanCurrent = cha_san[0].get("current");
-                cha_sanStart[0].set("current", cha_sanCurrent);
-                check = true;
+on("chat:message", function(msg) {
+    if(msg.type === "api" && msg.content === "!!daypassed"){
+        if(cha_ids.length > 0) {
+            for(i=0; i<cha_ids.length; i++) {
+                let cha_san = findObjs({type:"attribute", name:"san", characterid:cha_ids[i]});
+                let cha_sanCurrent = cha_san[0].get("current");
+
+                let cha_sanStart = findObjs({type:"attribute", name:"san_start", characterid:cha_ids[i]});
+
+                if (cha_sanStart[0] === undefined) {
+                    cha_name = findObjs({type:"character", _id:cha_ids[i]});
+                    chaObj = JSON.parse(JSON.stringify(cha_name))
+                    sendChat("system", `/w gm ${chaObj[0].name} 캐릭터의 '하루 시작 이성치'가 비어 있습니다. 작성한 후 다시 !!daypassed 명령어를 실행해 주세요.`)
+                    return;
+                } else {
+                    cha_sanStart[0].set({"current" : cha_sanCurrent});
+                }
             }
-            check === true ? text = "/w gm 하루가 지났습니다. 하루 시작 이성치가 초기화 되었습니다." : text = "/w gm daypassed API를 실행하지 못했습니다.";
-            sendChat("system", text);
+            text = "하루 시작 이성치가 초기화 되었습니다."
+        } else {
+            text = "플레이어 캐릭터가 없습니다."
         }
+        sendChat("system", `/w gm ${text}`);
     }
 });
 
-on("change:attribute", function (obj) {
-    if (obj.get("name") === "san") {
+on("change:attribute", function(obj) {
+    if(obj.get("name")==="san") {
         const cha_id = obj.get('_characterid');
-        const cha_sanStart = findObjs({type: "attribute", name: "san_start", characterid: cha_id})[0].get("current");
-        const cha_startIndefInsaneNum = Math.floor((cha_sanStart / 5) * 4);
-        const cha_attr = obj.get('current');
-        const cha = getObj('character', cha_id);
-        const cha_ctrlby = cha.get('controlledby');
-        if (cha_attr <= cha_startIndefInsaneNum && (!playerIsGM(cha_ctrlby) && cha_ctrlby !== "")) {
-            const cha_name = cha.get('name');
-            sendChat(cha_name, "장기적 광기가 발동됩니다");
+        const cha_sanStart = findObjs({type:"attribute", name:"san_start", characterid:cha_id})[0].get("current");
+        const cha_startIndefInsane = Math.floor((cha_sanStart/5)*4);
+        let ch_attr = obj.get('current');
+        let cha = getObj('character', cha_id);
+        let ctrlby = cha.get('controlledby');
+        if (ch_attr <= cha_startIndefInsane && (!playerIsGM(ctrlby) && ctrlby != "")) {
+            let cha_name = cha.get('name');
+            sendChat(cha_name, "**장기적 광기가 발동됩니다**");
         }
     }
 });
